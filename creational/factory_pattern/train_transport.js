d39f2a4d9ab6ed12e29a2ad5b0e1840cb9651091
Wahba91 <zizo@makeitsimple.co.uk>
2021-07-04 16:19:34 +0200

    remove old factory codes

--- creational/factory_pattern/train_transport.js
+++
@@ -1,6 +0,0 @@
-module.exports = function () {
-  // Implement the interface (means here in JS the agreed skeleton) functions.
-  this.deliver = function () {
-    console.log('Deliver from train class');
-  }
-}
--- creational/factory_pattern/try.js
+++
@@ -1,8 +0,0 @@
-// Import the super class.
-var Transport_Factory = require('./transport_factory');
-
-var shipTransprt = Transport_Factory('Ship');
-var trainTransport = Transport_Factory('Train');
-
-shipTransprt.deliver();
-trainTransport.deliver();
